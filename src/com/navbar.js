d64ac2842e07d4030181c17c00803fb703eac8ef
FaizNaeem <faiznaeem192022@gmail.com>
2023-09-10 20:58:28 +0500

    hello

--- src/com/navbar.js
+++ src/com/navbar.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-
 export default function Navbar() {
   return (
     <div>    <nav class="navbar navbar-expand-lg navbar-light bg-light">
